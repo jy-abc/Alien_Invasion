7b362c6ea10a6c95e621f0b4836f238ffac0de76
jy-abc <1098989533@qq.com>
2025-11-16 16:50:37 +0800

    Update alien.py

--- Alien_Invasion/alien.py
+++ Alien_Invasion/alien.py
@@ -7,7 +7,7 @@ class Alien(Sprite):
         super().__init__()
         self.screen=ai_game.screen
         self.settings=ai_game.settings
-        self.image=pygame.image.load(r"D:\app\vscode\Alien_Invasion\alien.bmp")
+        self.image=pygame.image.load(r"alien.bmp")
         
         self.rect=self.image.get_rect()
         self.rect.x=self.rect.width
@@ -23,4 +23,5 @@ class Alien(Sprite):
     def check_edges(self):
         '''检查外星人是否到边缘'''
         screen_rect=self.screen.get_rect()
+
         return (self.rect.right>=screen_rect.right) or (self.rect.left<=0)
