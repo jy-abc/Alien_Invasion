740f175c02d549a28b697b9c41ae7bc33e1d5923
jy-abc <1098989533@qq.com>
2025-11-18 22:14:20 +0800

    Update ship.py

--- Alien_Invasion/ship.py
+++ Alien_Invasion/ship.py
@@ -10,7 +10,7 @@ class Ship(Sprite):
         self.screen=ai_game.screen
         self.settings=ai_game.settings
         
-        self.image=pygame.image.load(r"Alien_Invasion\ship_photo.bmp")
+        self.image=pygame.image.load(r"ship_photo.bmp")
         self.rect=self.image.get_rect() #获得图像矩形
         self.screen_rect=ai_game.screen.get_rect()  #获得屏幕矩形
         
@@ -52,4 +52,5 @@ class Ship(Sprite):
         self.y=float(self.rect.y)
 
         
+
 
